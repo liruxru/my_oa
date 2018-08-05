6df7fbc1c1f7b67b5705a880038e78a022631e29
liruxru <1176881129@qq.com>
2018-08-05 23:27:29 +0800

    activity 工作流集成 https://www.activiti.org/5.x/userguide/#_eclipse_indigo_and_juno jar包引用低版本报updateScheame报错 采用稍高版本

---
+++ src/test/java/com/bonc/activity/Demo.java
@@ -0,0 +1,30 @@
+package com.bonc.activity;
+
+import org.activiti.engine.*;
+import org.junit.Test;
+
+public class Demo {
+    /**
+     * 使用默认配置文件 activiti.cfg.xml 获取核心api
+     */
+    @Test
+    public void  test(){
+        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+        System.out.println(processEngine);
+    }
+
+    /**
+     * API测试
+     */
+    @Test
+    public void testAPI(){
+        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+        RuntimeService runtimeService = processEngine.getRuntimeService();
+        RepositoryService repositoryService = processEngine.getRepositoryService();
+        TaskService taskService = processEngine.getTaskService();
+        ManagementService managementService = processEngine.getManagementService();
+        IdentityService identityService = processEngine.getIdentityService();
+        HistoryService historyService = processEngine.getHistoryService();
+        FormService formService = processEngine.getFormService();
+    }
+}
